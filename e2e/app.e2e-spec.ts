fab08c8d92f4306d67528f3313e8291f4b1b1f80
remzGrges <leander.coevoet@student.kdg.be>
2018-02-03 21:37:40 +0100

    Test CodeShip pls work

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,5 +1,7 @@
-import { Page } from './app.po';
+import {Page} from './app.po';
 import {browser} from "protractor";
+import {timeout} from "q";
+import {timeInterval} from "rxjs/operator/timeInterval";
 
 describe('App', () => {
   let page: Page;
@@ -12,13 +14,15 @@ describe('App', () => {
     beforeEach(() => {
       page.navigateTo('/');
       browser.waitForAngularEnabled(true);
-      setTimeout(1000);
+      
     });
 
     it('FUCK THIS have a title saying Ionic App', () => {
-      page.getTitle().then(title => {
-       expect(title).toEqual('Ionic App');
-     });
+      setTimeout(() => {
+        page.getTitle().then(title => {
+          expect(title).toEqual('Ionic App');
+        });
+      }, 1000);
     });
 
     it('should have a title saying Ionic App', () => {
