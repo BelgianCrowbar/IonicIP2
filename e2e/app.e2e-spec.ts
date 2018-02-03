b839f3ffe2ef1abb0f75b0a53291d4086d87d5fb
remzGrges <leander.coevoet@student.kdg.be>
2018-02-03 22:15:02 +0100

    Test CodeShip pls work

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -11,19 +11,18 @@ describe('App', () => {
 
   describe('default screen', () => {
     beforeEach(() => {
-      browser.get('/');
+
       page.navigateTo('/');
-      browser.waitForAngularEnabled(true);
     });
 
     it('FUCK THIS have a title saying Ionic App', () => {
-      page.getTitle().then(title => {
+      browser.getTitle().then(title => {
         expect(title).toEqual('Ionic App');
       });
     });
 
     it('should have a title saying Ionic App', () => {
-      page.getTitle().then(title => {
+      browser.getTitle().then(title => {
         expect(title).toEqual('Ionic App');
       });
     });
