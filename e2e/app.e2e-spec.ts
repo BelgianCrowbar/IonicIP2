74666176e7542882b5f6fc0971ad833be9618020
remzGrges <leander.coevoet@student.kdg.be>
2018-02-03 21:56:30 +0100

    Test CodeShip pls work

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,5 @@
 import {Page} from './app.po';
 import {browser} from "protractor";
-import {titleCase} from "@ionic/app-scripts";
 
 describe('App', () => {
   let page: Page;
@@ -12,22 +11,24 @@ describe('App', () => {
 
   describe('default screen', () => {
     beforeEach(() => {
-
       browser.get('/');
-     // page.navigateTo('/');
+      page.navigateTo('/');
+      browser.waitForAngularEnabled(true);
     });
 
     it('FUCK THIS have a title saying Ionic App', () => {
-    browser.getTitle().then(title => {
+    browser.
+      browser.getTitle().then(title => {
       expect(title).toEqual('Ionic App');
     })
 
     });
 
-   /* it('should have a title saying Ionic App', () => {
+    it('should have a title saying Ionic App', () => {
       page.getTitle().then(title => {
         expect(title).toEqual('Ionic App');
       });
-    });*/
+    });
+
   })
 });
