ae3128afcde31472f768d952cbd211545440a79d
remzGrges <leander.coevoet@student.kdg.be>
2018-03-15 11:20:42 +0100

    Heroku link

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-//export const SERVER_URL = "https://springip2.herokuapp.com/";
-export const SERVER_URL = "http://localhost:8080/";
+export const SERVER_URL = "https://springip2.herokuapp.com/";
+//export const SERVER_URL = "http://localhost:8080/";
